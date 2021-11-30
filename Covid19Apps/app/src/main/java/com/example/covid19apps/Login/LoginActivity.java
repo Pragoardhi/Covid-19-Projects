c806d04e80fc0b30b87b3ae7dcaaabad2d7365e5
Pragoardhi <44479434+Pragoardhi@users.noreply.github.com>
2021-11-30 08:25:08 +0700

    Update LoginActivity.java

--- Covid19Apps/app/src/main/java/com/example/covid19apps/Login/LoginActivity.java
+++ Covid19Apps/app/src/main/java/com/example/covid19apps/Login/LoginActivity.java
@@ -73,6 +73,9 @@ public class LoginActivity extends AppCompatActivity {
             if(check){
                 Login();
             }
+            else{
+                pd.cancel();
+            }
         });
     }
 
